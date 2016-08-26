51e56577bd7cb0a68c8ab1cda1718effd178990f
Zsolt <Zsolt@DESKTOP-OK5J989>
2016-08-26 17:03:38 +0200

    Add project files.

---
+++ BrowserTest/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Windows;
+
+namespace BrowserTest
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            PropertyInfo activeXProp = Browser.GetType().GetProperty("ActiveXInstance", BindingFlags.Instance | BindingFlags.NonPublic);
+            object activeX = activeXProp.GetValue(Browser, null);
+            activeX.GetType().GetProperty("Silent").SetValue(activeX, true);
+        }
+    }
+}
